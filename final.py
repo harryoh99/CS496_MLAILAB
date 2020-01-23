345dd4727646728a2d8e340b62688b31418542bb
Jio <harryoh99@naver.com>
2020-01-23 19:18:38 +0900

    fifth

--- final.py
+++ final.py
@@ -3,7 +3,7 @@ import torch
 import os
 import torch.nn as nn
 import torch.optim as optim
-from torch.autograd import Variable
+from torch.utils.tensorboard import SummaryWriter
 from torch.utils.data import Dataset, DataLoader
 from torch.utils.data.sampler import SubsetRandomSampler
 import matplotlib.pyplot as plt
@@ -23,13 +23,13 @@ class re_data(Dataset):
         return self.len
 
 def save_data():
-    torch.save(model, 'crt1_model.pt')
     torch.save(model2, 'crt2_model.pt')
+    torch.save(model_dropout, 'crt2_model_dropout.pt')
 
 def load_data():
-    model = torch.load('crt1_model.pt')
     model2 = torch.load('crt2_model.pt')
-    return model, model2
+    model_dropout = torch.load('crt2_model_dropout.pt')
+    return model2, model_dropout
 
 
 #63560, 15, 667 for x
@@ -37,15 +37,6 @@ def load_data():
 #For year 1
 
 #Model
-model = nn.Sequential(
-    nn.Linear(667,300),
-    nn.ReLU(),
-    nn.Linear(300,100),
-    nn.ReLU(),
-    nn.Linear(100,20),
-    nn.ReLU(),
-    nn.Linear(20,1)
-)
 model2 = nn.Sequential(
     nn.Linear(667,300),
     nn.ReLU(),
@@ -55,9 +46,22 @@ model2 = nn.Sequential(
     nn.ReLU(),
     nn.Linear(20,1)
 )
+model_dropout = nn.Sequential(
+    nn.Dropout(p=0.2),
+    nn.Linear(667,300),
+    nn.ReLU(),
+    nn.Dropout(p=0.5),
+    nn.Linear(300,100),
+    nn.ReLU(),
+    nn.Dropout(p=0.5),
+    nn.Linear(100,20),
+    nn.ReLU(),
+    nn.Dropout(p=0.5),
+    nn.Linear(20,1)
+)
 
 #Criterion
-criterion = nn.MSELoss()
+#criterion = nn.MSELoss()
 
 def criterion2(output, target):
     res = torch.abs(output-target)/target
@@ -76,7 +80,7 @@ for i in range(1,15):
     y_data = torch.cat((y_data, temp_y), dim=0)
 
 dataset_size = x_data.shape[0] #(63560*15,667)
-validation_split = 0.1
+validation_split = 0.01
 
 indices = list(range(dataset_size))
 split = int(np.floor(validation_split * dataset_size))
@@ -94,26 +98,20 @@ test_dataset = re_data(test_x,test_y)
 batch_size = 50
 
 train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle = True)
-validation_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, shuffle = True)
+validation_loader = torch.utils.data.DataLoader(test_dataset,  shuffle = True)
 
 learning_rate = 0.01
-optimizer = optim.Adam(model.parameters(), lr = learning_rate)
+optimizer_dropout= optim.Adam(model2.parameters(), lr = learning_rate)
 optimizer2 = optim.Adam(model2.parameters(), lr = learning_rate)
-num_epoch = 10
+num_epoch = 50
 
 #Training loss saved here
-training_loss =[]
-training2_loss =[]
 
-for epoch in range(num_epoch):
-    #Training
-    for batch_index, (data,target) in enumerate(train_loader):
-        optimizer.zero_grad()
-        output = model(data)
-        loss = criterion(output, target)
-        loss.backward()
-        optimizer.step()
-        training_loss.append([epoch,round(loss.item(),3)])
+training2_loss =[]
+training2_epoch_loss = []
+
+training2_dropout_loss = []
+training2_dropout_epoch_loss =[]
 
 for epoch in range(num_epoch):
     #Training
@@ -124,53 +122,63 @@ for epoch in range(num_epoch):
         loss.backward()
         optimizer2.step()
         training2_loss.append([epoch,round(loss.item(),3)])
+    training2_epoch_loss.append(round(loss.item(),3))
+
+for epoch in range(num_epoch):
+    #Training
+    for batch_index, (data,target) in enumerate(train_loader):
+        optimizer_dropout.zero_grad()
+        output = model_dropout(data)
+        loss = criterion2(output, target)
+        loss.backward()
+        optimizer_dropout.step()
+        training2_dropout_loss.append([epoch,round(loss.item(),3)])
+    training2_dropout_epoch_loss.append(round(loss.item(),3))
 
 
 save_data()
-model,model2 = load_data()
+model2, model_dropout = load_data()
 #Tested with criterion 2
-loss21_list = []
-loss22_list = []
+loss_list = []
+loss_dlist = []
 
 with torch.no_grad():
     for batch_index, (data,target) in enumerate(validation_loader):
-        output1 = model(data) #Trained with criterion1
         output2 = model2(data) #Trained with criterion 2
-
-        loss21 = criterion2(output1,target) #TRained with criterion 1, tested with 2
+        output_dropout = model_dropout(data)
         loss22 = criterion2(output2,target) #Trainde with criterion 2, tested with 2
-        loss21_list.append(loss21.item())
-        loss22_list.append(loss22.item())
+        loss_dropout = criterion2(output_dropout,target)
+        loss_list.append(loss22.item())
+        loss_dlist.append(loss_dropout.item())
 
-loss21_avg = np.average(loss21_list)
-loss22_avg = np.average(loss22_list)
 
+loss_avg = np.average(loss_list)
+loss_dropout_avg = np.average(loss_dlist)
 #Write the average
 f = open("output.txt","w")
-f.write('Training Loss with criterion 1: \n')
-f.write(" ".join(training_loss.__str__()))
 
 f.write("\nTraining Loss with criterion 2: \n")
 f.write(" ".join(training2_loss.__str__()))
-
-f.write("\nAverage loss of model trained with criterion 1\n")
-f.write(str(loss21_avg))
+f.write("\nTraining Loss with criterion 2 with dropout: \n")
+f.write(" ".join(training2_dropout_loss.__str__()))
 f.write("\nAverage loss of model trained with criterion 2\n")
-f.write(str(loss22_avg))
+f.write(str(loss_avg))
+f.write("\nAverage loss of model trained with criterion 2 with dropout\n")
+f.write(str(loss_dropout_avg))
 
 f.close()
 
 
 
-plt.figure()
-plt.title("Test Results on each training")
-plt.xlabel("test count")
-plt.ylabel("loss")
-plt.plot(loss21_list, marker = 'o', label = 'Trained with crt 1, test with crt 2', color = 'blue')
-plt.plot(loss22_list, marker = 'x', label = 'Trained with crt 2, test with crt 2', color = 'pink')
-plt.legend() 
-plt.savefig('test_result.png')
-plt.close()
+writer = SummaryWriter("./logs/check")
+for idx in range(len(loss_list)):
+    writer.add_scalars('test result', {'without dropout:': loss_list[idx], 
+                                        'with dropout: ': loss_dlist[idx]},idx)
+for idx in range(len(training2_epoch_loss)):
+    writer.add_scalars('training result for each epoch', {'without dropout': training2_epoch_loss[idx], 
+                                                            'with dropout': training2_dropout_epoch_loss[idx]}, idx)
+writer.close()
+
 
 
 
